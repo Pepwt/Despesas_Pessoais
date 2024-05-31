b827cdc2928f1754e096d10fa67dcbe31aba095f
Pepwt <pa632004@gmail.com>
2024-05-30 22:23:25 -0300

    Extraindo Widget

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -36,49 +36,13 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('R\$${tr.value.toStringAsFixed(2)}'),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 480 ? 
-                  TextButton.icon(
-                    onPressed: () => onRemove(tr.id),
-                    icon: Icon(Icons.delete, color: Color(0xFFCC0000)),
-                    label: Text(
-                      'Excluir',
-                      style: TextStyle(color: Color(0xFFCC0000)),
-                    ),
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(Color(0xFFCC0000)),
-                    ),   
-                  )
-                  : IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Color(0xFFCC0000),
-                    onPressed: () => onRemove(tr.id),
-                  ),
-                ),
-              );
+              return TransactionItem(
+                tr: tr, 
+                onRemove: onRemove,
+                );
             },
           );
   }
 }
+
+
